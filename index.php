98fe95100e15f7dafdfad39b5abcd1daa8073b3b
12miracahyati <115429054+12miracahyati@users.noreply.github.com>
2023-06-29 23:37:48 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Toko Sepatu | Mira</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <div class="header">
+    <p>Toko Sepatu | Mira</p>
+  </div>
+  <nav>
+    <ul> <?php include("page/navbar.php") ?> </ul>
+  </nav>
+
+  <?php include("content.php") ?>
+
+  <?php include("page/user/keranjang.php") ?>
+  <div class="footer">
+    <p>copyright by | mira | Repost by <a href="" target="_blank">Framework Id</a></p>
+  </div>
+
+</body>
+
+</html>
